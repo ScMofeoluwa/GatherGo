54378ae75c4a0ea41276f05d862a1b91de5b0204
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2023-10-14 01:57:07 +0100

    refactor: made little changes

--- internal/domain/user/repository/user_repository.go
+++ internal/domain/user/repository/user_repository.go
@@ -4,37 +4,37 @@ import (
 	"context"
 
 	"github.com/ScMofeoluwa/GatherGo/internal/domain/user/entity"
-	db "github.com/ScMofeoluwa/GatherGo/internal/infrastructure/database/sqlc"
+	"github.com/ScMofeoluwa/GatherGo/internal/infrastructure/database/sqlc"
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgtype"
 )
 
 type UserRepository struct {
-	Queries *db.Queries
+	sqlc.BaseRepository
 }
 
 var _ UserRepositoryInterface = &UserRepository{}
 
-func NewUserRepository(queries *db.Queries) *UserRepository {
-	return &UserRepository{Queries: queries}
+func NewUserRepository(db *sqlc.Queries) *UserRepository {
+	return &UserRepository{BaseRepository: sqlc.BaseRepository{DB: db}}
 }
 
-func (r *UserRepository) Create(ctx context.Context, user *entity.User) error {
-	params := db.CreateUserParams{
+func (u *UserRepository) Create(ctx context.Context, user *entity.User) error {
+	params := sqlc.CreateUserParams{
 		Email:    user.Email,
 		Password: user.Password,
 	}
 
-	err := r.Queries.CreateUser(ctx, params)
+	err := u.DB.CreateUser(ctx, params)
 	return err
 }
 
-func (r *UserRepository) GetByID(ctx context.Context, id string) (*entity.User, error) {
-	u, err := uuid.Parse(id)
+func (u *UserRepository) GetByID(ctx context.Context, id string) (*entity.User, error) {
+	uuid_, err := uuid.Parse(id)
 	if err != nil {
 		return nil, err
 	}
-	dbUser, err := r.Queries.GetUserByID(ctx, u)
+	dbUser, err := u.DB.GetUserByID(ctx, uuid_)
 	if err != nil {
 		return nil, err
 	}
@@ -47,13 +47,13 @@ func (r *UserRepository) GetByID(ctx context.Context, id string) (*entity.User, 
 	}, nil
 }
 
-func (r *UserRepository) Update(ctx context.Context, user *entity.User) error {
-	params := db.UpdateUserParams{
+func (u *UserRepository) Update(ctx context.Context, user *entity.User) error {
+	params := sqlc.UpdateUserParams{
 		Email:    pgtype.Text{String: user.Email},
 		Password: pgtype.Text{String: user.Password},
 		Verified: pgtype.Bool{Bool: user.Verified},
 	}
 
-	err := r.Queries.UpdateUser(ctx, params)
+	err := u.DB.UpdateUser(ctx, params)
 	return err
 }
