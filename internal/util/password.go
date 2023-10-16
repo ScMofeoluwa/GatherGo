b97ccbbd78109ceecfec5b5f76cbdc5bb5fd2fe6
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2023-10-16 15:10:06 +0100

    feat: hashing using bcrypt

---
+++ internal/util/password.go
@@ -0,0 +1,17 @@
+package util
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashedPassword), nil
+}
+
+func CheckPassword(hashedPassword, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
