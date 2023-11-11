76ddf768b7f5fa786d299ad84c8e875cbbb5fe28
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2023-11-11 20:26:58 +0100

    refactor: introduced code duplication for decoupling

--- internal/domain/user/entity/user_entity.go
+++ internal/domain/user/entity/user_entity.go
@@ -11,6 +11,6 @@ type User struct {
 }
 
 type CreateUser struct {
-	Password string `json:"password" binding:"required,min=6"`
-	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password"`
+	Email    string `json:"email"`
 }
--- internal/transport/http/handlers/auth_handler.go
+++ internal/transport/http/handlers/auth_handler.go
@@ -8,6 +8,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type SignUpRequest struct {
+	Password string `json:"password" binding:"required,min=6,max=255"`
+	Email    string `json:"email" binding:"required,email"`
+}
+
 type AuthHandler struct {
 	service *service.UserService
 }
@@ -25,12 +30,14 @@ func (a *AuthHandler) RegisterAuthRoutes(router *gin.RouterGroup) {
 }
 
 func (a *AuthHandler) SignUp(ctx *gin.Context) {
-	var req entity.CreateUser
+	var req SignUpRequest
 	if err := ctx.ShouldBindJSON(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, NewApiError(err.Error()))
 		return
 	}
-	if err := a.service.SignUp(ctx, &req); err != nil {
+
+	user := createUserFromReq(req)
+	if err := a.service.SignUp(ctx, &user); err != nil {
 		ctx.JSON(http.StatusBadRequest, NewApiError(err.Error()))
 		return
 	}
@@ -38,14 +45,23 @@ func (a *AuthHandler) SignUp(ctx *gin.Context) {
 }
 
 func (a *AuthHandler) SignIn(ctx *gin.Context) {
-	var req entity.CreateUser
+	var req SignUpRequest
 	if err := ctx.ShouldBindJSON(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, NewApiError(err.Error()))
 	}
-	data, err := a.service.SignIn(ctx, &req)
+
+	user := createUserFromReq(req)
+	data, err := a.service.SignIn(ctx, &user)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, NewApiError(err.Error()))
 		return
 	}
 	ctx.JSON(http.StatusOK, NewApiResponse(data, "login successful"))
+}
+
+func createUserFromReq(r SignUpRequest) entity.CreateUser {
+	return entity.CreateUser{
+		Email:    r.Email,
+		Password: r.Password,
+	}
 }
