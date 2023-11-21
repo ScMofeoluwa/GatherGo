dbfbb5114dc24c5dcdc98b025e70a3a8a3d12642
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2023-11-21 23:56:47 +0100

    refactor: folder director for auth handler

--- internal/transport/http/handlers/auth/auth_handler.go
+++ internal/transport/http/handlers/auth/auth_handler.go
@@ -1,10 +1,13 @@
-package handlers
+package auth
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/ScMofeoluwa/GatherGo/internal/domain/user/entity"
+	"github.com/ScMofeoluwa/GatherGo/internal/domain/user/repository"
 	"github.com/ScMofeoluwa/GatherGo/internal/domain/user/service"
+	"github.com/ScMofeoluwa/GatherGo/internal/transport/http/handlers"
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,10 +17,10 @@ type SignUpRequest struct {
 }
 
 type AuthHandler struct {
-	service *service.UserService
+	service service.UserServiceInterface
 }
 
-func NewAuthHandler(service *service.UserService) *AuthHandler {
+func NewAuthHandler(service service.UserServiceInterface) *AuthHandler {
 	return &AuthHandler{service: service}
 }
 
@@ -32,31 +35,43 @@ func (a *AuthHandler) RegisterAuthRoutes(router *gin.RouterGroup) {
 func (a *AuthHandler) SignUp(ctx *gin.Context) {
 	var req SignUpRequest
 	if err := ctx.ShouldBindJSON(&req); err != nil {
-		ctx.JSON(http.StatusBadRequest, NewApiError(err.Error()))
+		ctx.JSON(http.StatusBadRequest, handlers.NewApiError(err.Error()))
 		return
 	}
 
 	user := createUserFromReq(req)
 	if err := a.service.SignUp(ctx, &user); err != nil {
-		ctx.JSON(http.StatusBadRequest, NewApiError(err.Error()))
+		if errors.Is(err, service.ErrEmailAlreadyExists) {
+			ctx.JSON(http.StatusForbidden, handlers.NewApiError(err.Error()))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, handlers.NewApiError(err.Error()))
 		return
 	}
-	ctx.JSON(http.StatusCreated, NewApiResponse(nil, "user created successfully"))
+	ctx.JSON(http.StatusCreated, handlers.NewApiResponse(nil, "user created successfully"))
 }
 
 func (a *AuthHandler) SignIn(ctx *gin.Context) {
 	var req SignUpRequest
 	if err := ctx.ShouldBindJSON(&req); err != nil {
-		ctx.JSON(http.StatusBadRequest, NewApiError(err.Error()))
+		ctx.JSON(http.StatusBadRequest, handlers.NewApiError(err.Error()))
 	}
 
 	user := createUserFromReq(req)
 	data, err := a.service.SignIn(ctx, &user)
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, NewApiError(err.Error()))
+		if errors.Is(err, repository.ErrUserNotFound) {
+			ctx.JSON(http.StatusNotFound, handlers.NewApiError(err.Error()))
+			return
+		}
+		if errors.Is(err, service.ErrInvalidDetails) {
+			ctx.JSON(http.StatusUnauthorized, handlers.NewApiError(err.Error()))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, handlers.NewApiError(err.Error()))
 		return
 	}
-	ctx.JSON(http.StatusOK, NewApiResponse(data, "login successful"))
+	ctx.JSON(http.StatusOK, handlers.NewApiResponse(data, "login successful"))
 }
 
 func createUserFromReq(r SignUpRequest) entity.CreateUser {
